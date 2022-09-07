0463fbdf02294a3b9f8b6fc7795526be73d0811c
JR <jarichards123@gmail.com>
2022-09-07 11:34:42 +0100

    calc average

---
+++ calc-average.js
@@ -0,0 +1,13 @@
+// function find_average(array) {
+//     // your code here
+//     return 0;
+//   }
+
+//solution
+
+function find_average(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce((prev, curr) => prev + curr);
+}
